7343c175e21e009b62a8cc75ddb5329591a99cce
TheRocker7421 <76410930+TheRocker7421@users.noreply.github.com>
2021-08-21 17:14:46 -0500

    One more issue fixed.

--- super-octocat-windows.bat
+++ super-octocat-windows.bat
@@ -1,5 +1,5 @@
 @echo off
 set "RESPONSE="
-echo "Hello! Welcome to Super Octocat."
+echo Hello! Welcome to Super Octocat.
 set /p response=What is your name? 
 set /p lang=What is your favorite programming language? 
