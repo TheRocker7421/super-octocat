12f575b8c70e773f7fc6199de21702103c7c03af
TheRocker7421 <76410930+TheRocker7421@users.noreply.github.com>
2021-08-21 17:09:49 -0500

    Update super-octocat-macos.sh

--- super-octocat-macos.sh
+++ super-octocat-macos.sh
@@ -1,4 +1,8 @@
 echo "Hello! Welcome to Super Octocat."
 echo
+echo "What is your name?"
 echo -n "Your name: "
 read -r name
+echo "What is your favorite programming language?"
+echo -n "Your answer: "
+read -r lang
