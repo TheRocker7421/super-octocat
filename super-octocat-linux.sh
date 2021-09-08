a1c8f274c93c1e4557f1d32aa03ecac9690f57a1
TheRocker7421 <76410930+TheRocker7421@users.noreply.github.com>
2021-09-08 17:25:54 -0500

    Delete super-octocat-linux.sh

--- super-octocat-linux.sh
+++
@@ -1,9 +0,0 @@
-echo "Hello! Welcome to Super Octocat."
-echo
-echo "What is your name?"
-echo -n "Your answer: "
-read -r name
-echo "What is your favorite programming language?"
-echo -n "Your answer: "
-read -r lang
-echo Welcome on board, Octocat $name
